a0dc576b27e56424e415f3b49f915eb52544ef41
Rizky Sugiharto <rizkysugiharto132@gmail.com>
2025-07-21 22:03:30 +0700

    Fix statistics's output, algoritm, and sql queries

--- api/v1/stats.js
+++ api/v1/stats.js
@@ -26,7 +26,7 @@ function getWhereExpressionByDateRange(value) {
         'last month': 'created_at >= DATE_SUB(CURDATE(), INTERVAL 1 MONTH)',
         'last year': 'created_at >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)',
         'last 3 years': 'created_at >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR)',
-    }[value] ?? 'BY DAY(created_at)'
+    }[value] ?? 'created_at >= DATE_SUB(CURDATE(), INTERVAL 1 WEEK)'
 }
 
 function convertToValidData(dateRange, data, noneData) {
@@ -36,13 +36,17 @@ function convertToValidData(dateRange, data, noneData) {
         'last year': 12,
         'last 3 years': 36,
     }[dateRange]
-    const newData = []
+    const newData = new Array(num).fill(null)
 
     for (let i = 0; i < num; i++) {
-        newData.push({
-            index: i+1,
-            ...(data[i] ?? noneData)
-        })
+        newData[i] = {
+            index: i + 1,
+            ...noneData
+        }
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        newData[data[i].index - 1] = data[i]
     }
 
     return newData
@@ -124,10 +128,14 @@ module.exports = function (fastify, opts, done) {
         preHandler: [fastify.authenticate]
     }, async (req, reply) => {
         const dateRange = getValidDateRange(req.query.date_range)
+        const whereExpression = getWhereExpressionByDateRange(dateRange)
+        const groupByExpression = getGroupByExpressionByDateRange(dateRange)
         const result = (await fastify.mysql.query(
-            `SELECT total_cost AS sales FROM transactions
-            WHERE type = 'sale' AND ${getWhereExpressionByDateRange(dateRange)}
-            GROUP BY ${getGroupByExpressionByDateRange(dateRange)}`
+            `SELECT
+                ${groupByExpression} AS \`index\`,
+                total_cost AS sales FROM transactions
+            WHERE type = 'sale' AND ${whereExpression}
+            GROUP BY ${groupByExpression}`
         ))[0]
             
         result.forEach((item) => {
@@ -146,26 +154,20 @@ module.exports = function (fastify, opts, done) {
         const whereExpression = getWhereExpressionByDateRange(dateRange)
         const groupByExpression = getGroupByExpressionByDateRange(dateRange)
         const result = (await fastify.mysql.query(
-            `SELECT (
-                SELECT COUNT(*) FROM transactions
-                WHERE ${whereExpression} AND type = 'purchase'
-                GROUP BY ${groupByExpression}
-            ) AS purchase, (
-                SELECT COUNT(*) FROM transactions
-                WHERE ${whereExpression} AND type = 'sale'
-                GROUP BY ${groupByExpression}
-            ) AS sale, (
-                SELECT COUNT(*) FROM transactions
-                WHERE ${whereExpression} AND type = 'return'
-                GROUP BY ${groupByExpression}
-            ) AS _return`
+            `SELECT
+                ${groupByExpression} AS \`index\`,
+                SUM(CASE WHEN type = "purchase" THEN 1 ELSE 0 END) AS purchase,
+                SUM(CASE WHEN type = "sale" THEN 1 ELSE 0 END) AS sale,
+                SUM(CASE WHEN type = "return" THEN 1 ELSE 0 END) AS \`return\`
+            FROM transactions
+            WHERE ${whereExpression}
+            GROUP BY ${groupByExpression}`
         ))[0]
 
         result.forEach((item) => {
-            item.purchase = item.purchase ?? 0
-            item.sale = item.sale ?? 0
-            item.return = item._return ?? 0
-            delete item._return
+            item.purchase = parseInt(item.purchase) ?? 0
+            item.sale = parseInt(item.sale) ?? 0
+            item.return = parseInt(item.return) ?? 0
         })
         
         return reply.code(HttpStatusCode.Ok).send(convertToValidData(dateRange, result, {
@@ -181,7 +183,6 @@ module.exports = function (fastify, opts, done) {
         const dateRange = getValidDateRange(req.query.date_range)
         const whereExpression = getWhereExpressionByDateRange(dateRange)
         const groupByExpression = getGroupByExpressionByDateRange(dateRange)
-        let result
 
         const mostUsageTransactionItems = (await fastify.mysql.query(
             `SELECT COUNT(*) AS _usage, product_id, unit_name FROM transaction_items
@@ -191,9 +192,11 @@ module.exports = function (fastify, opts, done) {
         ))[0][0]
 
         const stockLogs = (await fastify.mysql.query(
-            `SELECT init_stock AS stock FROM stock_logs
-                WHERE ${whereExpression} AND product_id = ?
-                GROUP BY ${groupByExpression}
+            `SELECT
+                ${groupByExpression} AS \`index\`,
+                init_stock AS stock FROM stock_logs
+            WHERE ${whereExpression} AND product_id = ?
+            GROUP BY ${groupByExpression}
             `,
             [mostUsageTransactionItems.product_id]
         ))[0]
